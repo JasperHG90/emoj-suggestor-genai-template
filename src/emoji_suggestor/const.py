75cac355f5cb77a76673558f7e80b92465b9a957
Jasper Ginn <jasperginn@gmail.com>
2024-04-08 12:53:25 +0200

    chore: clarify prompt

--- src/emoji_suggestor/const.py
+++ src/emoji_suggestor/const.py
@@ -1,6 +1,6 @@
 TEMPLATE = """
-Task: Given the following user's text, suggest the most appropriate emoji that represents the sentiment or main idea of the message.
-You should only return the emoji and no additional text.
+Task: Given the following user's text, suggest the single most appropriate emoji that represents the sentiment or main idea of the message.
+You should only return a single emoji and no additional text.
 
 Examples:
 Input: Shielding VMs from the internet
